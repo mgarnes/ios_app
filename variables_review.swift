ad577e24430df8e648290efe0a1c6ccdd5617d35
Morgan Matthew D. Garnes <76693488+mgarnes@users.noreply.github.com>
2021-07-10 21:31:27 -0400

    first review

---
+++ variables_review.swift
@@ -0,0 +1,17 @@
+/* Review from Codecademy
+ * This is a body mass indicator calculator
+ */
+
+// This will be the final result of the user's BMI
+var bmi: Double
+
+// My weight (140 lbs) in kg
+var weight = 63.5
+// My height (5'9) in m
+var height = 1.75
+
+// Calculations portion
+bmi = weight/(height*height)
+
+// Prints my current BMI
+print("My current BMI is \(bmi).")
