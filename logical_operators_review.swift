509e73d1421f134483d4745bb5535f8eb44b95bb
Morgan Matthew D. Garnes <76693488+mgarnes@users.noreply.github.com>
2021-07-11 21:15:42 -0400

    third review

---
+++ logical_operators_review.swift
@@ -0,0 +1,7 @@
+/* Review from Codecademy
+ * The logical && operator translates to AND and returns true only when both operands are true. It returns false in all other cases.
+ * The logical || operator translates to OR and returns true when either operands are true. When both operands are false, the logical expression evaluates to false.
+ * The logical ! operator translates to NOT and negates a Boolean value.
+ * The && operator has a higher precedence over the || operator.
+ * Parentheses, (), may be used in a logical expression to control the order of operations and improve readability.
+ */
