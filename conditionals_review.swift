44748cb685431583d72fdbd88868d30d04705147
Morgan Matthew D. Garnes <76693488+mgarnes@users.noreply.github.com>
2021-07-11 21:11:38 -0400

    second review

---
+++ conditionals_review.swift
@@ -0,0 +1,10 @@
+/* Review from Codecademy
+ * An if statement consists of a condition and code block that executes when the condition is true.
+ * An else statement is immediately followed by a code block that executes when all previous conditions were false.
+ * A code block is denoted by a set of curly braces {}.
+ * Multiple else if statements can be chained within an if/else to provide additional conditions.
+ * Comparison operators include <, >, <=, >=, ==, and != and are used to compare the values of two operands.
+ * A switch statement looks for the value of a case that matches the value of an expression.
+ * A switch statement can have cases that contain multiple items known as compound cases.
+ * A switch statement’s case can include a range of values using the closed range operator (...).
+ */
